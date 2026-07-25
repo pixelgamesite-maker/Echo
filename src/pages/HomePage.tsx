627b755d61ed474b7e2a03592b7c4250a4ab2591
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-07-25 17:10:34 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import { Hero } from "@/components/Hero";
 import { HowItWorks } from "@/components/HowItWorks";
 import { FeaturedAgents } from "@/components/FeaturedAgents";
 import { Categories } from "@/components/Categories";
-import { LiveMintFeed } from "@/components/LiveMintFeed";
 import { Collections } from "@/components/Collections";
 
 export default function HomePage() {
@@ -12,7 +11,6 @@ export default function HomePage() {
       <HowItWorks />
       <FeaturedAgents />
       <Categories />
-      <LiveMintFeed />
       <Collections />
     </>
   );
