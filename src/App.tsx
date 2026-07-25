25f7d897f140483cb22a3cad83bfcfe7171c5601
pixelgamesite-maker <pixelgamesite@gmail.com>
2026-07-25 10:02:27 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { wagmiConfig } from "./lib/wagmiConfig";
 import { Navigation } from "./components/Navigation";
-import { Hero } from "./components/Hero";
-import { MintWindow } from "./components/MintWindow";
-import { HowItWorks } from "./components/HowItWorks";
-import { FeaturedAgents } from "./components/FeaturedAgents";
-import { Categories } from "./components/Categories";
-import { LiveMintFeed } from "./components/LiveMintFeed";
-import { Collections } from "./components/Collections";
 import { Footer } from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import MintPage from "./pages/MintPage";
+import MyAgentsPage from "./pages/MyAgentsPage";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-cream text-ink font-sans">
-      <Navigation />
-      <Hero />
-      <MintWindow />
-      <HowItWorks />
-      <FeaturedAgents />
-      <Categories />
-      <LiveMintFeed />
-      <Collections />
-      <Footer />
-    </div>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <div className="min-h-screen bg-cream text-ink font-sans flex flex-col">
+            <Navigation />
+            <div className="flex-1">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/mint" element={<MintPage />} />
+                <Route path="/my-agents" element={<MyAgentsPage />} />
+              </Routes>
+            </div>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
